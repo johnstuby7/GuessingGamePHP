2a9f369cf895eeeab28f547f9e1bb2e0731045e7
johnstuby7 <johnstuby7@gmail.com>
2021-05-05 11:19:16 -0600

    pushing guessing game php to github

---
+++ guess.php
@@ -0,0 +1,36 @@
+<html>
+	<body>
+		<title> Guessing Game for John Stuby </title>
+		<h1> Welcome to my guessing game </h1>
+		<?php
+		$correctnumber = 22; // Number to Guess
+		if(isset($_GET['guess']))
+		{
+			if(is_numeric($_GET['guess']) === FALSE)
+			{
+				echo "Your guess is not a number";
+			}
+			else if($_GET['guess']<$correctnumber)
+			{
+				echo "Your guess is too low";
+			}
+			else if($_GET['guess']>$correctnumber)
+			{
+				echo "Your guess is too high";
+			}
+			else if($_GET['guess'] == $correctnumber)
+			{
+				echo "Congratulations - You are right";
+			}
+			else if($_GET['guess']<$correctnumber)
+			{
+				echo "Your guess is too low";
+			}
+			else
+			{
+				echo "Missing guess parameter <br> Your guess is too short";
+			}
+		}
+		?>
+	</body>
+</html>
